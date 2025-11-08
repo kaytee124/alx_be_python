91723b2a5e3cc346efb87c78e0347baed0f78da5
kaytee124 <ktproductions124@gmail.com>
2025-11-08 16:02:18 +0000

    fixed the name

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -10,3 +10,4 @@ print("Addition of" + number1 + "and" + number2 + "is:" + sum)
 print("Subtraction of" + number1 + "and" + number2 + " is:" + sub)
 print("Multiplication of" + number1 + "and" + number2 + "is:" + mul)
 print("Division of" + number1 + "and" + number2 + "is:" + div ) 
+
