07304f1a9255d2b00d6d0fa910430edabb1e8235
kaytee124 <ktproductions124@gmail.com>
2025-11-23 19:13:55 +0000

    alx first work

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,18 @@
-def perform_operation(num1,num2,operation):
+# arithmetic_operations.py
+
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Perform basic arithmetic operations (add, subtract, multiply, divide) 
+    based on the input operation string.
+
+    Args:
+    num1 (float): The first number.
+    num2 (float): The second number.
+    operation (str): The arithmetic operation to perform ("add", "subtract", "multiply", "divide").
+
+    Returns:
+    float or str: The result of the operation, or an error message if division by zero or invalid operation.
+    """
     match operation:
         case "add":
             return num1 + num2
@@ -13,4 +27,3 @@ def perform_operation(num1,num2,operation):
                 return "Error: Division by zero"
         case _:
             return "Error: Invalid operation"
-    
