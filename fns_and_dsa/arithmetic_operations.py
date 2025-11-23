f63a2ad396a36179546655ea465afdc407cbbff3
kaytee124 <ktproductions124@gmail.com>
2025-11-23 19:21:53 +0000

    alx first work

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation):
-    if(operation == "add"):
+    if operation == "add":
         return num1 + num2
-    elif(operation == "subtract"):
+    elif operation == "subtract":
         return num1 - num2
-    elif(operation == "multiply"):
+    elif operation == "multiply":
         return num1 * num2
-    elif(operation == "divide"):
+    elif operation == "divide":
         if num2 == 0:
             return "Error: Division by zero"
         else:
